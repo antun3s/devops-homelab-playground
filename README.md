c483b28457e3265c203b7d3658c682cf6ea115af
Bruno Antunes <antunes.dll@gmail.com>
2024-11-28 21:46:03 -0300

    terraform funcional para criação de VMs que serao master e workers com tags

--- README.md
+++ README.md
@@ -3,4 +3,7 @@
 A ideia é utilizar o terraform para criar cluster kubernetes
 
 Videos de base do mateus Muller
-Link: https://www.youtube.com/watch?v=_vit-bn0LyI 
+Link: https://www.youtube.com/watch?v=_vit-bn0LyI
+
+repositorio de codigo do video:
+https://github.com/mateusmuller/mateusmullerme-youtube/tree/main/kubernetes/02_kubernetes_production_ready/terraform
---
+++ locals.tf
@@ -0,0 +1,78 @@
+locals {
+  # global configurations
+  agent        = 1
+  cidr         = "192.168.8.0/24"
+  onboot       = true
+  proxmox_node = "pve"
+  scsihw       = "virtio-scsi-pci"
+  template     = "ubuntu-2204-cloud-init"
+
+  bridge = {
+    interface = "vmbr0"
+    model     = "virtio"
+  }
+  disks = {
+    main = {
+      backup  = true
+      format  = "raw"
+      type    = "disk"
+      storage = "local-lvm"
+      slot    = "scsi0"
+    }
+    cloudinit = {
+      backup  = false
+      format  = "raw"
+      type    = "cloudinit"
+      storage = "local-lvm"
+      slot    = "ide2"
+    }
+  }
+  # serial is needed to connect via WebGUI console
+  serial = {
+    id   = 0
+    type = "socket"
+  }
+
+  # cloud init information to be injected
+  cloud_init = {
+    user           = "ubuntu"
+    password       = "ubuntu"
+    ssh_public_key = file("/home/antunes/.ssh/id_ed25519.pub")
+  }
+
+  # master specific configuration
+  masters = {
+    # how many nodes?
+    count = 1
+
+    name_prefix = "k8s-master"
+    vmid_prefix = 300
+
+    # hardware info
+    cores     = 2
+    disk_size = "23G"
+    memory    = 2048
+    sockets   = 1
+
+    # 192.168.0.7x and so on...
+    network_last_octect = 70
+    tags                = "masters"
+  }
+
+  # worker specific configuration
+  workers = {
+    count = 1
+
+    name_prefix = "k8s-worker"
+    vmid_prefix = 400
+
+    cores     = 1
+    disk_size = "23G"
+    memory    = 2048
+    sockets   = 1
+
+    network_last_octect = 90
+    tags                = "workers"
+  }
+}
+
